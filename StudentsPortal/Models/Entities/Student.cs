7ce7ac26124fec80b1bea3b05847dacdc5256d78
strini <naicker.strini7@gmail.com>
2024-05-18 14:30:51 +0200

    Add project files.

---
+++ StudentsPortal/Models/Entities/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentsPortal.Models.Entities
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string Phone { get; set; }
+
+        public bool Subscribed { get; set; }
+    }
+}
---
+++ StudentsPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentsPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
